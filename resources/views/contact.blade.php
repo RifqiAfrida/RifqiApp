6f18f15308253171a3132c5239ad897b392274ad
RifqiAfrida <rifqiebong2507@gmail.com>
2022-03-28 18:47:56 +0700

    Bagian Assets

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>Contact</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 </head>
 <body>
-    This Is Home Page.
+    Contact Page.
 
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', fn() => view('home'));
+
+Route::view('contact', 'contact');
+
+Route::view('posts/first-post', 'posts.show');
